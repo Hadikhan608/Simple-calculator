a010e9e40049799cf4f922cc6bd80daf6ccec96b
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-20 18:30:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    { message: "Enter first Number", type: "number", name: "firstNumber" },
+    { message: "Enter second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+if (answers.operator === "+") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "-") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "*") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "/") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Please select a valid operator");
+}
